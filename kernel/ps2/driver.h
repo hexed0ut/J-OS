edfe18ee72289309e50d8b3009199308879336ba
hexed0ut <102050935+hexed0ut@users.noreply.github.com>
2026-05-02 22:08:26 +0530

    update driver to integrate new keymap structure

--- kernel/ps2/driver.h
+++ kernel/ps2/driver.h
@@ -3,34 +3,33 @@
 
 #include "keymap.h"
 #include "state.c"
+#include "scan_code_set_1.h"
 
 unsigned char is_keypress() {
     return (inb(0x64) & 1);
 }
 
-char resolve_ps2_key(unsigned char inp) {
-    int length = sizeof(ps2_us_qwerty) / sizeof(ps2_us_qwerty[0]);
+char resolve_ps2_key(unsigned char scan_code) {
     
-    for (int i = 0; i < length; i++) {
-        if (inp == ps2_us_qwerty[i].scan_code) {
+    if (ps2_us_qwerty[scan_code].type == PRINTABLE) {
+        if (SHIFT_ACTIVE) {
+            return ps2_us_qwerty[scan_code].key - 32;
+        }
+        else {
+            return ps2_us_qwerty[scan_code].key;
+        }
+    }
 
-            if (ps2_us_qwerty[i].type == PRINTABLE) {
-                if (SHIFT_ACTIVE) {
-                    return ps2_us_qwerty[i].key - 32;
-                }
-                else {
-                    return ps2_us_qwerty[i].key;
-                }
-            }
-
-            else if (ps2_us_qwerty[i].type == MODIFIER) {
-                if (inp == 0x2A || inp == 0x36) {
-                    SHIFT_ACTIVE = 1;
-                }
-                else if (inp == 0xAA || inp == 0xB6) {
-                    SHIFT_ACTIVE = 0;
-                }
-            }
+    else if (ps2_us_qwerty[scan_code].type == MODIFIER) {
+        
+        if (scan_code == SC_LSHIFT_PRESS || scan_code == SC_RSHIFT_PRESS) {
+            SHIFT_ACTIVE = 1;
+        }
+        else if (scan_code == SC_LSHIFT_RELEASE || scan_code == SC_RSHIFT_RELEASE) {
+            SHIFT_ACTIVE = 0;
+        }
+        else {
+            return 0;
         }
     }
     return 0;
@@ -39,8 +38,8 @@ char resolve_ps2_key(unsigned char inp) {
 char await_ps2_key() {
     while (1) {
         if (is_keypress()) {
-            unsigned char inp = inb(0x60); 
-            return resolve_ps2_key(inp);
+            unsigned char scan_code = inb(0x60); 
+            return resolve_ps2_key(scan_code);
         }
     }
 }
