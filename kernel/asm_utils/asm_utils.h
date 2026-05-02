bd02198940680d7496f55ca5a40165e90c08a8c8
hexed0ut <102050935+hexed0ut@users.noreply.github.com>
2026-05-02 15:20:33 +0530

    update return type of inb function
    change return type of inb to unsigned char to allow values beyond 127

--- kernel/asm_utils/asm_utils.h
+++ kernel/asm_utils/asm_utils.h
@@ -6,8 +6,8 @@ void outb(int port, char value) {
     asm volatile("outb %0, %1" : :"a"(value), "Nd"(port));
 }
 
-char inb(int port) {
-    char ret;
+unsigned char inb(int port) {
+    unsigned char ret;
     asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
