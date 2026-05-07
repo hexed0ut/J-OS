4f104192d47db2f0e5604839bb52ce7dc4136a1a
regdocs <55305804+regdocs@users.noreply.github.com>
2026-05-07 19:27:43 +0530

    refactor: uppercase logic

--- lib/character.h
+++ lib/character.h
@@ -8,4 +8,17 @@ bool is_alpha( char ch )
 	return ('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z');
 }
 
+bool is_uppercase( char ch )
+{
+	return ('A' <= ch && ch <= 'Z');
+}
+
+char to_uppercase( char ch )
+{
+	if ( !is_uppercase( ch ) )
+		return ch - 32;
+
+	return ch;
+}
+
 #endif
--- lib/iostream.h
+++ lib/iostream.h
@@ -4,46 +4,49 @@
 #include "ps2/driver.h"
 #include "vga/cursor.h"
 #include "ps2/keymap.h"
-#include "string.h"
+#include "character.h"
 
-char resolve_shift( unsigned char key )
+char resolve_shift( unsigned char ch )
 {
-    if ( is_alpha( key ) )
-        return to_uppercase( key );
+	if ( is_alpha( ch ) )
+	{
+		if ( !CAPSLOCK_ACTIVE ) return to_uppercase( ch );
+		else return ch;
+	}
 
-    return shift_map[key];
+  return shift_map[ch];
 }
 
 char resolve_keycode( unsigned char keycode )
 {
-    if ( keycode_ascii[keycode].type == PRINTABLE )
-    {
-        if ( SHIFT_ACTIVE )
-            return resolve_shift( keycode_ascii[keycode].ascii );
+	struct KeyMapValue val = keycode_ascii[keycode];
 
-        if ( CAPSLOCK_ACTIVE )
-            return to_uppercase( keycode_ascii[keycode].ascii );
+	if ( val.type == PRINTABLE )
+	{
+	  if ( SHIFT_ACTIVE ) return resolve_shift( val.ascii );
 
-        return keycode_ascii[keycode].ascii;
-    }
-    
-    return 0;
+	if ( CAPSLOCK_ACTIVE ) return to_uppercase( val.ascii );
+
+	return val.ascii;
+  }
+
+  return 0;
 }
 
 void read_input()
 {
-    char* vga = (char*)VGA_BUFFER_START;
-    int pos = 0;
-    while ( 1 )
-    {
-        enum keycode key = await_ps2_key_event( 0 );
-        char ch = resolve_keycode( key );
-        if ( !ch ) continue;
+	char* vga = (char*)VGA_BUFFER_START;
+	int pos = 0;
+	while ( 1 )
+	{
+		enum keycode key = await_ps2_key_event( 0 );
+		char ch = resolve_keycode( key );
+		if ( !ch ) continue;
 
-        *(vga + pos) = ch;
-        pos = pos + 2;
-        shift_cursor( 1 );
-    }
+	*(vga + pos) = ch;
+	pos = pos + 2;
+	shift_cursor( 1 );
+  }
 }
 
 void read_key_event() { return; }
