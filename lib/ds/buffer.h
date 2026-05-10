a0b0e8d60803302323dad6c53daa36fef2c83a91
hexed0ut <102050935+hexed0ut@users.noreply.github.com>
2026-05-10 20:29:25 +0530

    add: buffer utils

---
+++ lib/ds/buffer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+void push_to_buffer( char* buffer, int buffer_size, char** buffer_head, char ch )
+{
+	**buffer_head = ch;
+	*buffer_head += 1;
+}
+
+void pop_from_buffer( char* buffer, int buffer_size, char** buffer_head )
+{
+	*(*buffer_head - 1) = 0;
+	*buffer_head -= 1;
+}
